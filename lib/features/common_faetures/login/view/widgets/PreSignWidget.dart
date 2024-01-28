31c55278666b08f1626ae736c8de5b6991be0bb5
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2024-01-28 23:00:30 +0200

    Adding Media Query

--- lib/features/common_faetures/login/view/widgets/PreSignWidget.dart
+++ lib/features/common_faetures/login/view/widgets/PreSignWidget.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -6,40 +7,43 @@ import '../screens/SignUp.dart';
 
 class PreSignUpWidget extends StatelessWidget {
   const PreSignUpWidget({super.key, required this.image, required this.userType});
- final String image;
- final String userType;
+
+  final String image;
+  final String userType;
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: GestureDetector(
-                  onTap: () => Get.off(SignUp()),
-                  child: Container(
-                    
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromARGB(20, 68, 100, 160),
-                    ),
-                    // height: 100,
-                    // width: 250,
-                    child: SvgPicture.asset(
-                      image,
-                      height: 200,
-                    ),
-                  ),
-                ),
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: screenWidth * 0.2),
+          child: GestureDetector(
+            onTap: () => Get.off(SignUp()),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(20, 68, 100, 160),
               ),
-               Text(
-                userType,
-                style: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF1E496B),
-                ),
-              )
-            ],
-          );
+              height: screenWidth * 0.4,
+              width: screenWidth * 0.8,
+              child: SvgPicture.asset(
+                image,
+                height: screenWidth * 0.35,
+              ),
+            ),
+          ),
+        ),
+        Text(
+          userType,
+          style: TextStyle(
+            fontSize: screenWidth * 0.07,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF1E496B),
+          ),
+        ),
+      ],
+    );
   }
 }
