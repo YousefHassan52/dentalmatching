58d1a6e9526cd1104a52d961bc8951c8689b807e
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2024-01-28 23:06:48 +0200

    Text Form After Media Query

--- lib/features/common_faetures/login/view/widgets/TextForm.dart
+++ lib/features/common_faetures/login/view/widgets/TextForm.dart
@@ -3,46 +3,61 @@ import 'package:flutter/material.dart';
 class TextForm extends StatelessWidget {
   final String hint;
   final IconData icon;
+  final IconData? icon2;
   final TextInputType? type;
   final bool password;
 
-  const TextForm(
-      {super.key,
-      required this.hint,
-      required this.icon,
-      this.type,
-      required this.password});
+  const TextForm({
+    super.key,
+    required this.hint,
+    required this.icon,
+    this.type,
+    required this.password,
+    this.icon2,
+  });
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:15),
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05 , vertical: screenWidth * 0.02),
       child: Form(
         child: TextFormField(
-            obscureText: password,
-            cursorColor: const Color(0xFF4464A0),
-            keyboardType: type,
-            style: const TextStyle(
-              color: Color(0xFF4464A0),
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
+          obscureText: password,
+          cursorColor: const Color(0xFF4464A0),
+          keyboardType: type,
+          style: TextStyle(
+            color: Color(0xFF4464A0),
+            fontWeight: FontWeight.bold,
+            fontSize: screenWidth * 0.04,
+          ),
+          decoration: InputDecoration(
+            prefixIcon: Icon(
+              icon,
+              color: const Color.fromARGB(224, 94, 148, 195),
+              size: screenWidth * 0.08,
             ),
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                icon,
-                color: const Color.fromARGB(224, 94, 148, 195),
-                size: 30,
-              ),
-              hintText: hint,
-              hintStyle: const TextStyle(
-                  color: Color.fromARGB(100, 94, 148, 195), fontSize: 20),
-              enabledBorder: const UnderlineInputBorder(
-                borderSide: BorderSide(width: 3, color: Color(0xFFCBE1F8)),
-              ),
-              focusedBorder: const UnderlineInputBorder(
-                borderSide: BorderSide(width: 3.5, color: Color(0xFF4464A0)),
-              ),
-            )),
+            suffixIcon: icon2 != null
+                ? Icon(
+                    icon2,
+                    color: const Color.fromARGB(224, 94, 148, 195),
+                    size: screenWidth * 0.08,
+                  )
+                : null,
+            hintText: hint,
+            hintStyle: TextStyle(
+              color: Color.fromARGB(100, 94, 148, 195),
+              fontSize: screenWidth * 0.04,
+            ),
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(width: 3, color: Color(0xFFCBE1F8)),
+            ),
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(width: 3.5, color: Color(0xFF4464A0)),
+            ),
+          ),
+        ),
       ),
     );
   }
