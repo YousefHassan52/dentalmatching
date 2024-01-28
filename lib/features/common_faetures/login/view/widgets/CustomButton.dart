90f8d816864c5415d1e95d9a8230d8cb6246ea2b
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2024-01-28 23:08:01 +0200

    Login Button

--- lib/features/common_faetures/login/view/widgets/CustomButton.dart
+++ lib/features/common_faetures/login/view/widgets/CustomButton.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
-  final  void Function()? onPressed;
+  final void Function()? onPressed;
+
   const CustomButton({
     super.key,
     required this.text,
-   this.onPressed,
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return SizedBox(
-      height: 50,
-      width:150 ,
+      height: screenWidth * 0.1,
+      width: screenWidth * 0.4,
       child: MaterialButton(
-        disabledColor: Color(0xFF1E496B),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        disabledColor: const Color(0xFF1E496B),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(screenWidth * 0.05)),
         onPressed: onPressed,
         textColor: Colors.white,
-        child: Text(text,style: TextStyle(fontSize: 25),),
-      
+        child: Text(
+          text,
+          style: TextStyle(fontSize: screenWidth * 0.05),
+        ),
       ),
     );
   }
