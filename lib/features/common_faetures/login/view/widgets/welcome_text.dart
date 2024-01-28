3b5eca302688b92e353d23842fd030d4156ff5d3
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2024-01-28 23:30:59 +0200

    heading

--- lib/features/common_faetures/login/view/widgets/welcome_text.dart
+++ lib/features/common_faetures/login/view/widgets/welcome_text.dart
@@ -1,19 +1,31 @@
-import 'package:dentalmatching/core/constants/styles.dart';
 import 'package:flutter/material.dart';
 
-class AuthWelcomeText extends StatelessWidget {
-  const AuthWelcomeText({
+class PreText extends StatelessWidget {
+  final String text;
+
+  const PreText({
     super.key,
-    required this.welcomeText,
+    required this.text,
   });
-  final String welcomeText;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      welcomeText,
-      style: Styles.textStyle16,
-      textAlign: TextAlign.center,
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: EdgeInsets.only(top: screenWidth * 0.15),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: const Color(0xFF1E496B),
+            fontSize: screenWidth * 0.08,
+            fontWeight: FontWeight.w800,
+            height: 1.2, 
+            letterSpacing: 2,
+          ),
+        ),
+      ),
     );
   }
 }
