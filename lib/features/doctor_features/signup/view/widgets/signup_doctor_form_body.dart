5b158e498b6dc608d01354321c5a4d837297ab66
YousefHassan52 <yousef52hm@gmail.com>
2024-03-09 20:50:51 +0200

    signup doctor screen responsive issue solved

--- lib/features/doctor_features/signup/view/widgets/signup_doctor_form_body.dart
+++ lib/features/doctor_features/signup/view/widgets/signup_doctor_form_body.dart
@@ -135,15 +135,23 @@ class SignupDoctorFormBody extends StatelessWidget {
           ),
           GetBuilder<SignupDoctorControllerImpl>(
             builder: (controller) => Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const AddCardImage(),
-                const Spacer(),
-                CircleAvatar(
-                  radius: ((Get.width * 0.3 - 40) / 2) - 5,
-                  backgroundColor: AppColors.mainColor,
-                  backgroundImage: controller.imageFile != null
-                      ? FileImage(controller.imageFile!)
-                      : null,
+                const Flexible(
+                  child:
+                      FittedBox(fit: BoxFit.scaleDown, child: AddCardImage()),
+                ),
+                Flexible(
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: CircleAvatar(
+                      radius: ((Get.width * 0.3 - 40) / 2) - 5,
+                      backgroundColor: AppColors.mainColor,
+                      backgroundImage: controller.imageFile != null
+                          ? FileImage(controller.imageFile!)
+                          : null,
+                    ),
+                  ),
                 ),
               ],
             ),
