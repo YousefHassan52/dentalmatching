4fe0d317e70be156127b6ea427de0d6427159bf8
YousefHassan52 <yousef52hm@gmail.com>
2024-03-23 04:04:06 +0200

    test commit

--- lib/features/admin_doctor_features/view_whole_case/view/upper_widget.dart
+++ lib/features/admin_doctor_features/view_whole_case/view/upper_widget.dart
@@ -10,7 +10,7 @@ class BioBarWidgetAdmin extends StatelessWidget {
   const BioBarWidgetAdmin({
     super.key,
   });
-
+// test commit
   @override
   Widget build(BuildContext context) {
     ViewWholeCaseForAdminControllerImp controller =
